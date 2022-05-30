8362d45eca92e5d1c66106be744cdd8d62de5c01
Vinayak <sareenv026@outlook.com>
2022-05-30 12:19:41 -0400

    changes

---
+++ src/main/java/com/teamj/VaccinationSystem/VaccinationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.teamj.VaccinationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccinationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamj/VaccinationSystem/VaccinationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamj.VaccinationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
